9478d971bea64d7f41d3b613bab473bc15a6e2e8
moosecoop <cooper@makhijani.us>
2017-05-07 16:26:26 -0700

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var expect = require('chai').expect;
-var prettyLog = require('../index');
-var moment = require('moment');
-var chalk = require('chalk');
-
-var output = prettyLog('Status set', false, false);
-expect (output).to.equal(`${chalk.bgBlue(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] LOG`)} ${chalk.white(`Status set`)}`);
