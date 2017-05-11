37b49243287c203a9d687b40cbfe5b9c0f6f92c4
Mr. Sakamoto <me@felis.cat>
2017-05-11 17:19:06 +0300

    hi new code

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-'use strict';
-var chalk = require('chalk');
-var moment = require('moment');
+const chalk = require('chalk');
+const moment = require('moment');
+const { inspect } = require('util');
 
-module.exports = function (data, error, log) {
-    if (error) {
-        if (log) {
-            console.log(`${chalk.bgRed(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ERR`)} ${chalk.italic(`${data}`)}`);
-        } else {
-            return `${chalk.bgRed(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ERR`)} ${chalk.italic(`${data}`)}`;
-        }
-    } else {
-        if (log) {
-            console.log(`${chalk.bgBlue(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] LOG`)} ${chalk.white(`${data}`)}`);
-        } else {
-            return `${chalk.bgBlue(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] LOG`)} ${chalk.white(`${data}`)}`;
-        }
+module.exports = class Logger {
+    static log(...args) {
+        this._log(0, ...args);
     }
-};
+
+    static info(...args) {
+        this._log(1, ...args);
+    }
+
+    static debug(...args) {
+        this._log(2, ...args);
+    }
+
+    static warn(...args) {
+        this._log(3, ...args);
+    }
+
+    static error(...args) {
+        this._log(4, ...args);
+    }
+    
+    static _log(level, ...args) {
+        const texts = this._cleanArgs(args);
+
+        const time = chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`);
+        const tag = chalk.bold(`[${this.LogLevel(level)}]:`);
+        const colored = chalk[this.LogColor(level)](texts);
+
+        if (level == 4) return process.stderr.write(`${time} ${tag} ${colored}\n`);
+        process.stdout.write(`${time} ${tag} ${colored}\n`);
+    }
+    
+    static _cleanArgs(args) {
+        const texts = [];
+
+        for (const arg of args) {
+            if (typeof arg == 'string') {
+                texts.push(arg);
+                continue;
+            }
+            texts.push(inspect(arg));
+        }
+        return texts.join(' ');
+    }
+    
+    static logLevel(level) {
+        return ['Log', 'Status', 'Debug', 'Error'][level];
+    }
+    
+    static logColor(level) {
+        return ['grey', 'green', 'yellow', 'red'][level];
+    }
+}
